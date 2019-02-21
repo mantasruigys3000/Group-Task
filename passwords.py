6099ec081081404fe4608475ccfe033eb496d3f6
unknown <mantasruigys101@gmail.com>
2019-02-21 20:33:30 +0000

    Added file paths

--- passwords.py
+++ passwords.py
@@ -8,7 +8,12 @@ for which they want to see the passwords for
 
 passwords = {} #dictionary of passwords with the users as the key
 
-def passSearch(lines): # function that takes list of lines from current open file
+def passSearch(lines,cPath):
+    """
+    function that takes list of lines from current open file
+        :param lines: the current list of lines to search through 
+        :param cPath: the path of the lines
+    """
     for line in lines:
 
         if line.find("Username",0,len(line)) > -1: #if the line contains username a variable is formatted from it to get just the username
@@ -20,9 +25,9 @@ def passSearch(lines): # function that takes list of lines from current open fil
             pName = pName[:-1]
 
             if uName in passwords.keys(): #checks to see if this user has already got a dictionary entry
-                passwords[uName].append(pName) # if so then add the found password to the list of passwords linked with the username
+                passwords[uName].append((pName,cPath)) # if so then add the found password to the list of passwords linked with the username
             else:
-                passwords[uName] = [pName] #if the name is not already a key make it one by creating a list with a single element so it can later be appended too
+                passwords[uName] = [[pName,cPath]] #if the name is not already a key make it one by creating a list with a single element so it can later be appended too
 
 print("Collecting Passwords...") # shows the program is working so the user does not think the program has crashed at the start
 
@@ -34,21 +39,27 @@ for root , dirs, files in os.walk("./L3"): # loops thorugh the entire folder of 
         if os.path.exists(fPath): # checks to see if the path can be accessed, should never return false as we created the file paths
             with open(fPath,"r") as text: 
                 try:
-                    passSearch(text.readlines()) # opens the file and passes through all the lines as a list into the passSearch function
+                    passSearch(text.readlines(),fPath) # opens the file and passes through all the lines as a list into the passSearch function
                 except Exception as e:
                     print("Error "+ str(e))
 
 
 
-def printPasswords(key): # macro for printing all passwords linked to a key
-    print("Passwords for user "+key+":\n"+str(passwords[key]))
+def printPasswords(key): 
+    """
+    macro for printing all passwords linked to a key
+        :param key: the username that you want the passwords for
+    """
+    print("Passwords for user "+key+":\n")
+    for p in passwords[key]:
+        print(p)
     
 print("Welcome to the password database, below are a list of users\ninput the user you wish to see the passwords of\nWrite exit to quit\n") #user prompt
 
 for k in passwords.keys(): # loop through every key in the dictionary and print it to give a list of users
     print(k)
 
-inputName = "" #black string in order to start the loop
+inputName = "" #blank string in order to start the loop
 
 while (inputName != "exit"):
 
